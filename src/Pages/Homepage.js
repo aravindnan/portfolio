f53160b97bedd9adc2c6fdf70617b2e5f7eb3963
Aravind <aravind812955033@gmail.com>
2023-05-23 23:39:27 +0530

    inital commit

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='mx-auto justify-center w-fit mt-[22%] text-xl text-slate-800 font-bold'>In development . . . </div>
+  )
+}
+
+export default Homepage
